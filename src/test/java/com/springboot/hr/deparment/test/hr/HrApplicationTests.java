16b44a9bfc53ff36b583d3418ebc0f905ecc2082
gustavo9601 <tavo9601@gmail.com>
2022-11-04 23:39:39 -0500

    init

---
+++ src/test/java/com/springboot/hr/deparment/test/hr/HrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hr.deparment.test.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
